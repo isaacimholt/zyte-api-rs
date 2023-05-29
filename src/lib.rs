43e4912c8f5c80ad68a680aec8939bd57d0a810e
Isaac Imholt <isaacimholt@users.noreply.github.com>
2023-05-30 01:37:24 +0200

    moved struct Request to request.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,14 @@
 //! # zyte-api-rs
 //!
 //! This is an unofficial rust package for the Zyte API.
+pub mod request;
+
 use base64::{engine::general_purpose, Engine as _};
 use http::Method;
-use serde::{Deserialize, Serialize, Serializer};
+use serde::{Deserialize};
 use std::error::Error;
 use std::str::FromStr;
-
-#[serde_with::skip_serializing_none]
-#[derive(Serialize, Debug, Default)]
-#[serde(rename_all = "camelCase")]
-pub struct Request {
-    #[serde(with = "http_serde::uri")]
-    url: http::Uri,
-    http_response_body: Option<bool>,
-    #[serde(serialize_with = "method_opt")]
-    http_request_method: Option<http::Method>,
-    #[serde(flatten)]
-    http_request_body_type: Option<HttpRequestBodyType>,
-}
-
-/// https://stackoverflow.com/a/76143471/196870
-fn method_opt<S: Serializer>(method_opt: &Option<http::Method>, ser: S) -> Result<S::Ok, S::Error> {
-    match method_opt {
-        Some(method) => http_serde::method::serialize(method, ser),
-        // This won't be encountered when using skip_serializing_none
-        None => ser.serialize_none(),
-    }
-}
-
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "camelCase")]
-pub enum HttpRequestBodyType {
-    HttpRequestBody(String), // todo: body should be bytes
-    HttpRequestText(String),
-}
+use request::{Request, HttpRequestBodyType};
 
 #[allow(dead_code)]
 #[derive(Deserialize, Debug)]
